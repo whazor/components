d8f08e74b3dcbf0977c4a4a5870952a63bd5e11e
Ruben Carvalho <rucarva@amazon.de>
2023-03-30 10:06:25 +0200

    chore: Add interface

--- src/container/interfaces.ts
+++ src/container/interfaces.ts
@@ -34,6 +34,15 @@ export interface ContainerProps extends BaseComponentProps {
   fitHeight?: boolean;
 
   /**
+   * Enabling this property will make the container to fit into available height. If content is too short, the container
+   * will stretch, if too long, the container will shrink and show vertical scrollbar.
+   *
+   * Use this property to align heights of multiple containers displayed in a single row. It is recommended to stretch
+   * all containers to the height of the longest one, to avoid extra vertical scroll areas.
+   */
+  image?: Image;
+
+  /**
    * Footer of the container.
    */
   footer?: React.ReactNode;
@@ -46,4 +55,20 @@ export interface ContainerProps extends BaseComponentProps {
    * @visualrefresh `stacked` variant
    */
   variant?: 'default' | 'stacked';
+}
+
+interface Image {
+  /**
+   * Specifies the URL of an image.
+   */
+  url?: string;
+  /**
+   * Specifies alternate text for the image (using the `url` attribute). We recommend that you provide this for accessibility.
+   * This property is ignored if you use a predefined icon or if you set your custom icon using the `svg` slot.
+   */
+  alt?: string;
+
+  height?: number;
+  width?: number;
+  position?: 'left' | 'right' | 'top' | 'bottom';
 }
