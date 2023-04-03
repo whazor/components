21b63edac5262e65b244f71746e64c2c2f0f5083
Ruben Carvalho <rucarva@amazon.com>
2023-04-03 11:18:56 +0000

    WIP

--- pages/container/media-container.page.tsx
+++ pages/container/media-container.page.tsx
@@ -1,5 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+// icon
+// demos with breakpoints / container queries
+
 import React from 'react';
 import Container from '~components/container';
 import Header from '~components/header';
@@ -9,51 +12,61 @@ import Input from '~components/input';
 import Select, { SelectProps } from '~components/select';
 import SpaceBetween from '~components/space-between';
 import ScreenshotArea from '../utils/screenshot-area';
-import dog from './assets/dog.png';
+import dog from './assets/video.png';
+import square from './assets/4-3.png';
+import icon from './assets/icon.png';
 import Box from '~components/box';
 import ColumnLayout from '~components/column-layout';
+// Images in the same row
+// Pick image per container
+// Pick object-fit
+//
 
-const ExampleContainer = () => (
-  <Container
-    image={{ position: 'top', src: dog, height: 200 }}
-    header={
-      <Header
-        variant="h2"
-        headingTagOverride="h1"
-        description={
-          <>
-            Some additional text{' '}
-            <Link fontSize="inherit" variant="primary">
-              with a link
-            </Link>
-            .
-          </>
-        }
-        info={<Link variant="info">Info</Link>}
-      >
-        Container with tag override
-      </Header>
-    }
-    footer={
-      <ExpandableSection headerText="Additional settings" variant="footer">
-        Place additional form fields here.
-      </ExpandableSection>
-    }
-  >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Phasellus tincidunt suscipit varius. Nullam dui tortor,
-    mollis vitae molestie sed, malesuada.Lorem ipsum dolor sit amet, consectetur adipiscing. Nullam dui tortor, mollis
-    vitae molestie sed. Phasellus tincidunt suscipit varius.
-  </Container>
-);
+const ExampleContainer = (props: any) => {
+  const { height, src } = props;
+  console.log(height);
+  return (
+    <Container
+      image={{ position: 'top', src: dog || dog }}
+      header={
+        <Header
+          variant="h2"
+          headingTagOverride="h1"
+          description={
+            <>
+              Some additional text{' '}
+              <Link fontSize="inherit" variant="primary">
+                with a link
+              </Link>
+              .
+            </>
+          }
+          info={<Link variant="info">Info</Link>}
+        >
+          Container with tag override
+        </Header>
+      }
+      footer={
+        <ExpandableSection headerText="Additional settings" variant="footer">
+          Place additional form fields here.
+        </ExpandableSection>
+      }
+    >
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Phasellus tincidunt suscipit varius. Nullam dui tortor,
+      mollis vitae molestie sed, malesuada.Lorem ipsum dolor sit amet, consectetur adipiscing. Nullam dui tortor, mollis
+      vitae molestie sed. Phasellus tincidunt suscipit varius.
+    </Container>
+  );
+};
 
 export default function SimpleContainers() {
-  const [height, setHeight] = React.useState('0');
+  const [height, setHeight] = React.useState(200);
   const [width, setWidth] = React.useState('0');
   const [selectedOption, setSelectedOption] = React.useState<SelectProps.Option | null>({
     label: 'cover',
     value: 'cover',
   });
-
+  console.log({ height });
   return (
     <article>
       <h1>Simple containers</h1>
@@ -66,7 +79,11 @@ export default function SimpleContainers() {
             </SpaceBetween>
             <SpaceBetween size="xs">
               <Box>Height (in px):</Box>
-              <Input type="number" onChange={({ detail }) => setHeight(detail.value)} value={height} />
+              <Input
+                type="number"
+                onChange={({ detail }) => setHeight(Number(detail.value))}
+                value={height.toString()}
+              />
             </SpaceBetween>
             <SpaceBetween size="xs">
               <Box>object-fit:</Box>
@@ -84,23 +101,24 @@ export default function SimpleContainers() {
               />
             </SpaceBetween>
           </SpaceBetween>
-          <ExampleContainer />
+
           <ColumnLayout columns={2}>
-            <ExampleContainer />
-            <ExampleContainer />
+            <ExampleContainer heigth={height} />
+            <ExampleContainer heigth={height} />
           </ColumnLayout>
           <ColumnLayout columns={3}>
-            <ExampleContainer />
-            <ExampleContainer />
-            <ExampleContainer />
+            <ExampleContainer heigth={height} />
+            <ExampleContainer src={square} heigth={height} />
+            <ExampleContainer heigth={height} />
           </ColumnLayout>
           <ColumnLayout columns={4}>
-            <ExampleContainer />
-            <ExampleContainer />
-            <ExampleContainer />
-            <ExampleContainer />
+            <ExampleContainer heigth={height} />
+            <ExampleContainer heigth={height} />
+            <ExampleContainer heigth={height} />
+            <ExampleContainer heigth={height} />
           </ColumnLayout>
-          <SpaceBetween size="l">
+          <ExampleContainer heigth={height} />
+          {/* <SpaceBetween size="l">
             <Container
               image={{ position: 'left', src: dog }}
               header={
@@ -131,7 +149,7 @@ export default function SimpleContainers() {
               tortor, mollis vitae molestie sed, malesuada.Lorem ipsum dolor sit amet, consectetur adipiscing. Nullam
               dui tortor, mollis vitae molestie sed. Phasellus tincidunt suscipit varius.
             </Container>
-          </SpaceBetween>
+          </SpaceBetween> */}
         </SpaceBetween>
       </ScreenshotArea>
     </article>
